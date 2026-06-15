3c2b9c19861ab74bb82820860e78dc915e670f83
altas-dev-io <258637815+atlas-dev-io@users.noreply.github.com>
2026-06-15 12:14:34 +0800

    feat(10_c++): add virtual function and dynamic polymorphism lab demos

---
+++ 10_c++/5.enumeration_in_class,const,static,virtual_function,polymorphism/7.virtual_function.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+  private:
+    int age;
+
+  public:
+    virtual void eat() { cout << "Animal is eatting" << endl; }
+};
+
+class Cat : public Animal {
+  public:
+    void eat() { cout << "Cat is eatting fish" << endl; }
+};
+
+class Dog : public Animal {
+  public:
+    void eat() { cout << "Dog is eatting bone" << endl; }
+};
+
+class Sheep : public Animal {
+  public:
+    void eat() { cout << "Sheep is eatting grass" << endl; }
+};
+
+class Pig : public Animal {
+  public:
+    void eat() { cout << "Pig is eatting cabbage" << endl; }
+};
+
+void show_animal_eat(Animal &other) { other.eat(); }
+
+int main() {
+    Animal a1;
+    Cat c1;
+    Dog d1;
+    Sheep s1;
+    Pig p1;
+
+    show_animal_eat(a1);
+    show_animal_eat(c1);
+    show_animal_eat(d1);
+    show_animal_eat(s1);
+    show_animal_eat(p1);
+
+    cout << "size of class Animal: " << sizeof(Animal) << endl;
+
+    return 0;
+}
